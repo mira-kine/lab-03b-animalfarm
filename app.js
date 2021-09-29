7ac349e6a9a8682a72661f30ae3bf172c432549a
mira-kine <mirak0419@gmail.com>
2021-09-29 15:34:47 -0700

    added three images and three sounds, clickable

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// import functions and grab DOM elements
+const horseImg = document.getElementById('horse-image');
+const horseSound = document.getElementById('horse');
+const dogImg = document.getElementById('dog-image');
+const dogSound = document.getElementById('dog');
+const catImg = document.getElementById('cat-image');
+const catSound = document.getElementById('cat');
 
-// initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+horseImg.addEventListener('click', ()=>{
+  horseSound.play();
+});
+
+dogImg.addEventListener('click', ()=>{
+  dogSound.play();
+});
+
+catImg.addEventListener('click', ()=>{
+  catSound.play();
+});
+
